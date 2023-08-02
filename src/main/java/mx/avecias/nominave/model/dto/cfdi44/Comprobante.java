52476d0785cb5cb1bbfc0b5235b4d67c04a3c4b9
Avecias <aveciasnash@gmail.com>
2023-08-01 19:25:44 -0500

    cambios en el acceso a los puertos

---
+++ src/main/java/mx/avecias/nominave/model/dto/cfdi44/Comprobante.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package mx.avecias.nominave.model.dto.cfdi44;
+
+import java.io.Serializable;
+import org.apache.commons.lang.builder.ReflectionToStringBuilder;
+import org.apache.commons.lang.builder.ToStringStyle;
+
+/**
+ *
+ * @author avecias
+ */
+public class Comprobante implements Serializable{
+    
+    private String version;
+
+    public Comprobante() {
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    @Override
+    public String toString() {
+        return ReflectionToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+    }
+    
+}
---
+++ src/test/java/test/cfdi40/TestXml.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package test.cfdi40;
+
+import mx.avecias.nominave.model.dto.cfdi44.Comprobante;
+
+/**
+ *
+ * @author avecias
+ * date: Aug 1, 2023 7:15:17 PM
+ */
+public class TestXml {
+    
+    public static void main(String[] args) {
+        System.out.println("cfdi version 4.0");
+        Comprobante comprobante = new Comprobante();
+        comprobante.setVersion("4.0");
+        System.out.println(comprobante);
+    }
+
+}
